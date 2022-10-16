08e9d97ed73d093e9566b0438e47136574406b04
fishman123456 <den9152@yandex.ru>
2022-10-16 22:09:41 +0700

    не сделан зубчатый массив и ввод оценки
    Весь день(Воскресенье писал) 22:07 Спать пора

--- Program.cs
+++ Program.cs
@@ -10,57 +10,81 @@
 // получение среднего балла по заданному предмету,
 // распечатка данных о студенте
 
-using System.Reflection;
-using System.Runtime.ExceptionServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Linq;
-
-Console.Title = "Класс студент"; // оформление
-Console.BackgroundColor = ConsoleColor.Black; //изменяет цвет фона
-Console.ForegroundColor = ConsoleColor.DarkGreen;//изменяет цвет текста
-Console.SetWindowSize(100, 30);//устанавливаем размер окна консоли
 
 class Program
 {
     static void Main()
     {
-        Student st = new Student();
+        Console.Title = "c#"; // оформление
+        Console.BackgroundColor = ConsoleColor.Black; //изменяет цвет фона
+        Console.ForegroundColor = ConsoleColor.DarkGreen;//изменяет цвет текста
+        Console.SetWindowSize(100, 30);//устанавливаем размер окна консоли
+        Student st = new Student("Ula", "Lov", "Kat", "Student", 25, 1000000);
+        st.Show();
+        Console.WriteLine("Вывод типа данных: " + st.GetType());
+        Console.WriteLine("Вывод случайной оценки: " + st.GetMark());
+        Student ula = new Student();
+        ula.Show();
+        ula.GetMark();
+        ula.Show();
+
 
     }
 }
+
 class Student // создаем класс студент
 {
-   public string _surname;//фамилия
-   public string _name;//имя
-   public int _patronymic;//отчество
-   public string _group;//группа
-   public int _age;// возраст
-   public double _estimation;//оценка
-    //public Student(string surname, string name, int patronymic,
-    //    string group, int age, double estimation)
-    //{
-    //    _surname = surname;
-    //    _name = name;
-    //    _patronymic = patronymic;
-    //    _group = group;
-    //    _age = age;
-    //    _estimation = estimation;
-    //}
+    private string _surname;//фамилия
+    private string _name;//имя
+    private string _patronymic;//отчество
+    private string _group;//группа
+    private int _age;// возраст
+    private double _estimation;//оценка
 
-    public Student(string surname, string name, int patronymic,
-        string group, int age, double estimation)
+
+    public Student() // конструктор без параметров/ по умолчанию
     {
-        this._surname = surname;
-        this._name = name;
-        this._patronymic = patronymic;
-        this._group = group;
-        this._age = age;
-        this._estimation = estimation;
+        _surname = "Ulia";
+        _name = "Loving";
+        _patronymic = "tigress";
+        _group = "Students";
+        _age = 16;
+        _estimation = 999;
+        Console.WriteLine("\nработает конструктор без параметрами\n");
+    }
+    public Student(string surname, string name, string patronymic,
+        string group, int age, double estimation)// конструктор с параметрами
+    {
+        this._surname = surname;        //  поле фамилия
+        this._name = name;              // поле имя
+        this._patronymic = patronymic;  // поле отчество
+        this._group = group;            // поле группа
+        this._age = age;                // поле возраст
+        this._estimation = estimation;  // поле оценка
         Console.WriteLine("\nработает конструктор с параметрами\n");
-        
+
+    }
+    public int GetMark()// возможность установки\получения оценок
+    {
+        return new Random().Next(1, 1000);
+    }
+    public static void SetMark(string setM)// возможность установки\получения оценок
+    {
+        Console.WriteLine("Ввод оценки: ");
+        Console.WriteLine("***********************************");
+        //setM = int.Parse(setM);
+        //_estimation =  setM;
     }
     public void Show() // Метод для вывода информации
     {
-        Console.WriteLine("ФИО : {0}", _surname, "\t", _name, "\t", _patronymic);
+        Console.WriteLine("ФИО : {0}", _surname + " " + _name + " " + _patronymic);
         Console.WriteLine("Группа: {0}", _group);
         Console.WriteLine("возраст :{0}", _age);
         Console.WriteLine("оценка : {0}", _estimation);
