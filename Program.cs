daa16b3c68e130203aa4871b197585b23ef34ee9
fishman123456 <den9152@yandex.ru>
2022-10-18 03:47:15 +0700

    доделал
    нотбук скоро сядет,

--- Program.cs
+++ Program.cs
@@ -22,21 +22,21 @@ class Program
 {
     static void Main()
     {
-        Console.Title = "c#"; // оформление
+        Console.Title = "*************"; // оформление
         Console.BackgroundColor = ConsoleColor.Black; //изменяет цвет фона
         Console.ForegroundColor = ConsoleColor.DarkGreen;//изменяет цвет текста
         Console.SetWindowSize(100, 30);//устанавливаем размер окна консоли
         Student st = new Student();
-      
-       // Console.WriteLine("Вывод типа данных: " + st.GetType());
-       // Console.WriteLine("Вывод случайной оценки: ");
+        // Console.WriteLine("Вывод типа данных: " + st.GetType());
+        // Console.WriteLine("Вывод случайной оценки: ");
         Student ula = new Student();
         ula.SetMar();
-        ula.Show();
         ula.GetMark();
+        ula.aver();
+        ula.Show();
+        st.SetMar();
+        st.aver();
         st.Show();
-        
-
     }
 }
 
@@ -47,19 +47,19 @@ class Student // создаем класс студент
     private string _patronymic;//отчество
     private string _group;//группа
     private int _age;// возраст
-    private int [][] _estimation = new int [3][];//оценка
+    private int[][] _estimation = new int[3][];//оценка
     private double _average_end; // средняя оценка
 
     public Student() // конструктор по умолчанию заполнение обьекта
     {
-        _surname = "Ulia";
         _name = "Loving";
         _patronymic = "tigress";
         _group = "Students";
         _age = 16;
-        _average_end = 9.2;
+        // _average_end = 9.2;
         Console.WriteLine("\nработает конструктор по умолчанию заполнение обьекта\n");
     }
+
     public Student(string surname, string name, string patronymic,
         string group, int age)// конструктор с параметрами
     {
@@ -70,64 +70,70 @@ class Student // создаем класс студент
         this._age = age;                // поле возраст
         Console.WriteLine("\nработает конструктор с параметрами\n");
     }
-    public void SetMar ()//  возможность установки оценок
+    public void SetMar()//  возможность установки оценок
     {
-        Console.WriteLine("Введите кол-во оценок\t");
+        Console.WriteLine("\tВведите кол-во оценок\t");
+
         int mass1 = int.Parse(Console.ReadLine()); // преобразуем строку в число
-        _estimation[0]=new int[mass1];  // выделяем память
-        Console.WriteLine("Оценки по программированию\t");
+
+        _estimation[0] = new int[mass1];  // выделяем память
+        Console.WriteLine("\tОценки по программированию\t");
         for (int i = 0; i < _estimation[0].Length; i++) // заполняем массив нулевой
         {
             _estimation[0][i] = int.Parse(Console.ReadLine()); // заносим переменные в массив
         }
+
         _estimation[1] = new int[mass1];
-        Console.WriteLine("Оценки по администрированию\t");
+        Console.WriteLine("\tОценки по администрированию\t");
         for (int i = 0; i < _estimation[1].Length; i++)
         {
             _estimation[1][i] = int.Parse(Console.ReadLine());
         }
+
         _estimation[2] = new int[mass1];
-        Console.WriteLine("Оценки по дизайну\t");
+        Console.WriteLine("\tОценки по дизайну\t");
         for (int i = 0; i < _estimation[2].Length; i++)
         {
             _estimation[2][i] = int.Parse(Console.ReadLine());
         }
     }
-    public  void GetMark()// возможность получения оценок и вывод на экран
+    public void GetMark()// возможность получения оценок и вывод на экран
     {
         Console.WriteLine("***********************************");
-        Console.WriteLine("Ввод оценки: ");
+        Console.WriteLine("\tВвод оценки: ");
 
-        for (int i = 0; i < _estimation[0].Length; i++) 
+        for (int i = 0; i < _estimation[0].Length; i++)
         {
-           Console.WriteLine("Оценки по программированию\t" + _estimation[0][i]) ; // вывод переменных из массива
+            Console.WriteLine("\tОценки по программированию\t" + _estimation[0][i]); // вывод переменных из массива
 
         }
-        
+
         for (int i = 0; i < _estimation[1].Length; i++)
         {
-            Console.WriteLine("Оценки по администрированию\t" + _estimation[1][i]); // вывод переменных из массива
+            Console.WriteLine("\tОценки по администрированию\t" + _estimation[1][i]); // вывод переменных из массива
         }
-       
+
         for (int i = 0; i < _estimation[2].Length; i++)
         {
-            Console.WriteLine("Оценки по дизайну\t" + _estimation[2][i]); // вывод переменных из массива
+            Console.WriteLine("\tОценки по дизайну\t" + _estimation[2][i]); // вывод переменных из массива
         }
         //setM = int.Parse(setM);
         //_estimation =  setM;
     }
+
     public void aver()
     {
         double average0 = _estimation[0].Average();
         double average1 = _estimation[1].Average();
         double average2 = _estimation[2].Average();
+        double _average_summ = average0 + average1 + average2;
+        Console.WriteLine("\tСумма оценок по 3 предметам\t" + _average_summ);
         _average_end = ((average0 + average1 + average2) / 3);
-        Console.WriteLine("Средняя оценка по 3 предметам\t" + _average_end);
-        
+        Console.WriteLine("\tСредняя оценка по 3 предметам\t" + _average_end);
     }
+
     public void Show() // Метод для вывода информации
     {
-       
         Console.WriteLine("ФИО : {0}", _surname + " " + _name + " " + _patronymic);
         Console.WriteLine("Группа: {0}", _group);
         Console.WriteLine("возраст :{0}", _age);
